1a08c7e9ef2a8097d1969bf3ef888ddfc4201716
Harikrishnan <harijkk@gmail.com>
2022-06-19 21:03:35 +0530

    update

--- biodimension/settings.py
+++ biodimension/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-CSRF_TRUSTED_ORIGINS = ['https://*.biodimension.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://www.biodimension.in']
 
 AUTH_PASSWORD_VALIDATORS = [
     {
